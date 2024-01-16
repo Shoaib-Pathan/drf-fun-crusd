8b167a2d6d7ed43bf7288bf2e06a908b88813f37
shoaib pathan <shoaib5792p@gmail.com>
2024-01-16 11:56:43 +0530

    modified views.py and urls.py

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,42 +1,22 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
-@api_view(['GET'])
-def show_view(request,pk=None):
-    id = pk
-    if id is not None:
-        data = Student.objects.get(id=id)
-        serialize_data = StudentSerializer(data)
-        return Response(serialize_data.data)
-    data = Student.objects.all()
-    serialize_data = StudentSerializer(data,many=True)
-    return Response(serialize_data.data)
+class Show_view(APIView):
+    def get(self,request):
+        data = Student.objects.all()
+        serialize_data = StudentSerializer(data,many=True)
+        return Response(serialize_data.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serialize_data = StudentSerializer(data=request.data)
+        if serialize_data.is_valid():
+            serialize_data.save()
+            return Response(serialize_data.data,status=status.HTTP_201_CREATED)
 
-@api_view(['POST'])
-def add_view(request):
-    serialize_data = StudentSerializer(data=request.data)
-    if serialize_data.is_valid():
-        serialize_data.save()
-        return Response(serialize_data.data)
-    return Response(serialize_data.errors)
 
-@api_view(['PUT'])
-def update_view(request,pk=None):
-    id = pk
-    data = Student.objects.get(id=id)
-    serialize_data = StudentSerializer(instance=data,data = request.data)
-    if serialize_data.is_valid():
-        serialize_data.save()
-        return Response(serialize_data.data)
-
-@api_view(['DELETE'])    
-def delete_view(request,pk=None):
-    id = pk
-    data = Student.objects.get(id=id)
-    data.delete()
-    return Response('{msg:data deleted successully}')
     
